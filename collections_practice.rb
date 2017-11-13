a784a57612d9633782770be4f78ae4ad2c53c835
Marvin Clerge <clergemarvin@gmail.com>
2017-11-13 12:17:56 +0000

    Did Enumerables sections, need to complete for hashes and nested data

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,20 @@
-# your code goes here
+def begins_with_r(arr)
+  arr.each {|x| next if x.start_with?("r") ; return false}
+  true
+end
+
+def contain_a(arr)
+  arr.select {|x| x.include?("a")}
+end
+
+def first_wa(arr)
+  arr.find {|x| x.to_s.include?("wa")}
+end
+
+def remove_non_strings(arr)
+  arr.delete_if {|x| (x.class != String)}
+end
+
+def count_elements(arr)
+
+end
